30e20848d0bc3675ec41243c19cc03bda2895a96
Simen Svenkerud <simensvenkerud@Simens-MacBook-Pro.local>
2024-01-23 16:35:23 +0100

    hitting head against wall

--- src/query_parser.py
+++ src/query_parser.py
@@ -1,3 +1,5 @@
+import sqlparse
+
 class QueryParser:
     def __init__(self, query):
         self.query = query
@@ -6,6 +8,8 @@ class QueryParser:
         self._parse()
 
     def _parse(self):
-        query_type, table = self.query.split()
-        self.query_type = query_type
-        self.table = [table]
+        query_type = sqlparse.parse(self.query)
+        parsed  = sqlparse.parse(self.query)
+        self.query_type = query_type[0].get_type()
+        print([str(t) for t in parsed[0].tokens if t.ttype is None][0])
+        print([t.ttype for t in parsed[0].tokens])
--- tests/test_query_parser.py
+++ tests/test_query_parser.py
@@ -1,9 +1,8 @@
-import pytest
 from src.query_parser import QueryParser
+
 
 def test_query_parser(simple_query):
     qp = QueryParser(simple_query)
     assert qp.query == simple_query
     assert qp.query_type == 'SELECT'
-    assert qp.table == ['TABLE_A']
-
+    assert qp.table == 'table1'
