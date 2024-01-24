50bd3cd648bab2c885d52dcad664d8db4a2c26df
Simen Svenkerud <simen.svenkerud@storebrand.no>
2024-01-24 15:35:40 +0100

    WIP

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,19 @@ import pytest
 def simple_query():
     query = """SELECT column_A FROM DB1.TABLE_A"""
     return query
+
+
 @pytest.fixture
 def lower_case_simple_query():
     query = """select column_A from table_A"""
     return query
 
+
 @pytest.fixture
 def drop_table_query():
     query = """DROP TABLE DEV_DB.TABLE_B"""
     return query
+
 
 @pytest.fixture
 def drop_database_query():
--- tests/test_query_parser.py
+++ tests/test_query_parser.py
@@ -8,6 +8,7 @@ def test_query_parser_simple(simple_query):
     assert qp.table == 'TABLE_A'
     assert qp.database == 'DB1'
 
+
 def test_simple_lower_case(lower_case_simple_query):
     qp = QueryParser(lower_case_simple_query)
     assert qp.query == lower_case_simple_query
@@ -15,12 +16,14 @@ def test_simple_lower_case(lower_case_simple_query):
     assert qp.table == 'table_A'
     assert qp.database == ""
 
+
 def test_drop_table(drop_table_query):
     qp = QueryParser(drop_table_query)
     assert qp.query == drop_table_query
     assert qp.query_type == 'DROP'
     assert qp.table == 'TABLE_B'
     assert qp.database == 'DEV_DB'
+
 
 def test_drop_database(drop_database_query):
     qp = QueryParser(drop_database_query)
