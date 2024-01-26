a44221d7fa8a58893b30fea7cd9f3c0ef4ec6079
Simen Svenkerud <simen.svenkerud@storebrand.no>
2024-01-26 18:55:49 +0100

    WIP

--- src/query_parser.py
+++ src/query_parser.py
@@ -25,9 +25,9 @@ class QueryParser:
             from_position = split_query.index('from')
         except:
             from_position = None
-        self.logger.debug(f"""from positio:{from_position}""")
+        self.logger.debug(f"""from position:{from_position}""")
 
-        if from_position:
+        if from_position :
             self.logger.debug(f"""source statement:{split_query[from_position + 1]}""")
             source_string = split_query[from_position + 1].replace('"', '')
             self.logger.debug(f"""source string:{source_string}""")
@@ -38,12 +38,19 @@ class QueryParser:
             source_string = split_query[2].replace('"', '').replace("'", '')
             self.source_object = (source_string.split('.'))
 
-        elif split_query[0].upper() in ['ALTER']:
-            if split_query[1].upper() == 'SESSION':
+        elif split_query[0].upper() in ['ALTER', 'SET']:
+            if split_query[1].upper() in ['SESSION']:
                 self.source_object = []
-            else:
+            elif split_query[1].upper() in ['TABLE']:
                 source_string = split_query[2].replace('"', '').replace("'", '')
                 self.source_object = (source_string.split('.'))
+            else:
+                source_string = split_query[1].replace('"', '').replace("'", '')
+                self.source_object = (source_string.split('.'))
+
+        elif split_query[0].upper() in ['REVOKE']:
+            source_string = split_query[4].replace('"', '').replace("'", '')
+            self.source_object = (source_string.split('.'))
 
         elif split_query[0].upper() in ['REMOVE']: 
             source_string = (split_query[1]
