e8d03a2bd0c61a3a606a6291825f6c4e535e35c2
Simen Svenkerud <simensvenkerud@desktop-7f5sf89.guest.tul.entra.no>
2024-02-07 18:03:14 +0100

    further cleanup

--- src/AzureKVCredentials.py
+++ src/AzureKVCredentials.py
@@ -1,11 +1,23 @@
 """Class containing methods to interact with Azure Key Vault to retrieve credentials."""
 
-from src.utils import az_secret
+from azure.identity import ManagedIdentityCredential
+from azure.keyvault.secrets import SecretClient
 
 
-def get_azure_credentials(logger, secret_name):
-    """Get the credentials from Azure Key Vault."""
+def get_azure_sf_credentials(logger, kv_url, managed_identity, secret_name):
+    """Get snowflake credentials form azure keywault.
+
+    Args: secret_name (str): Name of the secret
+    Args: kv_url (str): The url of the key vault
+    Args: managed_identity (str): The managed identity to use
+    Args: logger (logging.logger): The logger to use
+
+    Returns: str: The secret
+    """
+    credential = ManagedIdentityCredential(client_id=managed_identity)
+    secret_client = SecretClient(vault_url=kv_url, credential=credential)
     logger.info(f"Getting credentials for {secret_name}")
-    credentials = az_secret(secret_name)
+    snowflake_Credentials = secret_client.get_secret(secret_name).value
+
     logger.info(f"Credentials for {secret_name} retrieved")
-    return credentials
+    return snowflake_Credentials
